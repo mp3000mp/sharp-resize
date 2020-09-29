a476f3dbd7be40b8ef9921b8747f086f1340c532
Matthias Perret <moussadedijon@gmail.com>
2020-09-29 23:20:42 +0200

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# table-tr-href
+Simple tool based on [Sharp](https://github.com/lovell/sharp) that allow you to easily resize pictures.
+
+
+Table of Contents
+-----------------
+
+ - [Installation](#installation)
+ - [Usage](#usage)
+
+
+Installation
+------------
+
+```sh
+npm i
+```
+
+
+Usage
+-----
+
+- Put the pictures to be resized in the `input` folder.
+- Change `resizeTo` variable in `index.js`
+ 
+```sh
+npm run start
+```
+
+- Find your resized pictures in `output` folder.
---
+++ index.js
@@ -0,0 +1,50 @@
+let sharp = require('sharp');
+let fs = require('fs');
+let path = require('path');
+
+
+let resizeTo = 200; // px
+let srcFolder = './input';
+let destFolder = './output';
+let i = 0;
+
+
+// check folders exist
+if(fs.existsSync(srcFolder)){
+    if(fs.existsSync(destFolder)){
+
+        // pour chaque fichier
+        fs.readdir(srcFolder, function (err, files) {
+
+            if(files.length === 0){
+                console.log('No files found in ' + srcFolder);
+                process.exit();
+            }
+
+            // resize
+            files.forEach(function (file, index) {
+
+                let srcFilePath = path.join(srcFolder, file);
+                let destFilePath = path.join(destFolder, file);
+
+                sharp(srcFilePath)
+                    .resize(resizeTo)
+                    .toFile(destFilePath, (err, info) => {
+                        if(err){
+                            console.log('ERROR => ' + srcFilePath + ' => ' + destFilePath);
+                            console.log(err);
+                            console.log(info);
+                        }else{
+                            console.log('OK => ' + srcFilePath + ' => ' + destFilePath);
+                        }
+
+                    });
+                i++;
+            });
+        });
+    }else{
+        console.log('Folder ' + destFolder + ' unfound.');
+    }
+}else{
+    console.log('Folder ' + srcFolder + ' unfound.');
+}
